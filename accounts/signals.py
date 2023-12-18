b648ed29b8ed7c831d6fa58b71902e1d0717d9fe
cr1deg0 <crisidego@gmail.com>
2023-12-18 18:03:37 +0000

    add survey CRUD permissions to the user at user creation

---
+++ accounts/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User, Permission
+
+@receiver(post_save, sender=User)
+def set_user_permission(sender, instance, created, **kwargs):
+  """ Add general survey CRUD permissions to the user """
+  if created:
+    instance.user_permissions.add(Permission.objects.get(name='Can add survey'))
+    instance.user_permissions.add(Permission.objects.get(name='Can change survey'))
+    instance.user_permissions.add(Permission.objects.get(name='Can delete survey'))
+    instance.user_permissions.add(Permission.objects.get(name='Can view survey'))
+    print(instance.user_permissions.all())
