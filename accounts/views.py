0329cf2d2da311a46a739e19437e59a9f420c72c
cr1deg0 <crisidego@gmail.com>
2024-01-09 10:52:14 +0000

    formatting

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
-from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
 
 class SignUpView(CreateView):
-  form_class = UserCreationForm
-  template_name = 'registration/signup.html'
-  success_url = reverse_lazy('login')
+    form_class = UserCreationForm
+    template_name = "registration/signup.html"
+    success_url = reverse_lazy("login")
