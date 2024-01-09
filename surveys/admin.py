b35292b5f7ac20497a52a52659a547148aeceffb
cr1deg0 <crisidego@gmail.com>
2024-01-09 11:08:23 +0000

    formatting

--- surveys/admin.py
+++ surveys/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from guardian.admin import GuardedModelAdmin
-from .models import Survey, Question, Option
 
-# Register your models here.
+from .models import Option, Question, Survey
 
 
 class OptionInLine(admin.TabularInline):
-    """ Display Options in tabular mode"""
+    """Display Options in tabular mode"""
+
     model = Option
     extra = 0
 
@@ -18,14 +18,18 @@ class QuestionInLine(admin.StackedInline):
 
 @admin.register(Survey)
 class SurveyAdmin(GuardedModelAdmin):
-    """ Display survey title and creation date in admin surveys summary. Display Survey and Questions 
-    and together """
-    prepopulated_fields = {'slug': ('title',), }
+    """Display survey title and creation date in admin surveys summary. Display Survey and Questions
+    and together"""
+
+    prepopulated_fields = {
+        "slug": ("title",),
+    }
     inlines = [QuestionInLine]
-    list_display = ['title', 'created']
+    list_display = ["title", "created"]
 
 
 @admin.register(Question)
 class QuestionAdmin(GuardedModelAdmin):
-    """ Display Question and Options together """
+    """Display Question and Options together"""
+
     inlines = [OptionInLine]
--- surveys/signals.py
+++ surveys/signals.py
@@ -1,47 +1,25 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from guardian.shortcuts import assign_perm
-from surveys.models import Survey, Question, User
+
+from surveys.models import Question, Survey, User
 
 
 @receiver(post_save, sender=Survey)
 def set_survey_permission(sender, instance, created, **kwargs):
-    """ Add specific survey view, update and delete permissions to the author on survey instance creation """
+    """Add specific survey view, update and delete permissions to the author on survey instance creation"""
 
     if created:
-        assign_perm(
-            "view_own_survey",
-            instance.author,
-            instance
-        )
-        assign_perm(
-            "edit_own_survey",
-            instance.author,
-            instance
-        )
-        assign_perm(
-            "delete_own_survey",
-            instance.author,
-            instance
-        )
+        assign_perm("view_own_survey", instance.author, instance)
+        assign_perm("edit_own_survey", instance.author, instance)
+        assign_perm("delete_own_survey", instance.author, instance)
+
 
 @receiver(post_save, sender=Question)
 def set_question_permission(sender, instance, created, **kwargs):
-    """ Add specific question view, change and delete permission to the author on question instance creation """
+    """Add specific question view, change and delete permission to the author on question instance creation"""
     author = User.objects.get(survey_author__question_survey__id=instance.id)
     if created:
-        assign_perm(
-            "view_own_question",
-            author,
-            instance
-        )
-        assign_perm(
-            "edit_own_question",
-            author,
-            instance
-        )
-        assign_perm(
-            "delete_own_question",
-            author,
-            instance
-        )
+        assign_perm("view_own_question", author, instance)
+        assign_perm("edit_own_question", author, instance)
+        assign_perm("delete_own_question", author, instance)
