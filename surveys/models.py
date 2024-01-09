5e6f12e0db7de1f25987ca7589a199370d31f5a9
cr1deg0 <crisidego@gmail.com>
2024-01-09 11:06:06 +0000

    formatting

--- surveys/models.py
+++ surveys/models.py
@@ -1,12 +1,12 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
 
 
 class Survey(models.Model):
-    """ Model of a Survey. Default status at creation is DRAFT and slug is
-    added automatically. """
+    """Model of a Survey. Default status at creation is DRAFT and slug is
+    added automatically."""
 
     STATUS = [
         ("DRAFT", "Draft"),
@@ -14,36 +14,36 @@ class Survey(models.Model):
     ]
     title = models.CharField(max_length=250, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='survey_author')
+        User, on_delete=models.CASCADE, related_name="survey_author"
+    )
     slug = models.SlugField(max_length=100, unique=True)
     created = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(
-        max_length=6, choices=STATUS, default=STATUS[0][0])
+    status = models.CharField(max_length=6, choices=STATUS, default=STATUS[0][0])
     submissions = models.IntegerField(default=0)
 
     def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
+        self.slug = slugify(self.title)
         return super().save(*args, **kwargs)
 
     def get_absolute_url(self):
-        return reverse('survey_edit', args=[self.slug])
+        return reverse("survey_edit", args=[self.slug])
 
     def __str__(self):
         return self.title
 
     class Meta:
-        ordering = ['-created']
+        ordering = ["-created"]
         permissions = (
-            ('view_own_survey', 'Can view own survey'),
-            ('edit_own_survey', 'Can edit own survey'),
-            ('delete_own_survey', 'Can delete own survey'),
-            )
+            ("view_own_survey", "Can view own survey"),
+            ("edit_own_survey", "Can edit own survey"),
+            ("delete_own_survey", "Can delete own survey"),
+        )
 
 
 class Question(models.Model):
     survey = models.ForeignKey(
-        Survey, on_delete=models.CASCADE, related_name='question_survey')
+        Survey, on_delete=models.CASCADE, related_name="question_survey"
+    )
     question = models.CharField(max_length=250)
 
     def __str__(self):
@@ -51,16 +51,19 @@ class Question(models.Model):
 
     class Meta:
         permissions = (
-            ('view_own_question', 'Can view own question'),
-            ('edit_own_question', 'Can edit own question'),
-            ('delete_own_question', 'Can delete own question'),
+            ("view_own_question", "Can view own question"),
+            ("edit_own_question", "Can edit own question"),
+            ("delete_own_question", "Can delete own question"),
         )
+
     def get_absolute_url(self):
         return reverse("survey_edit", kwargs={"slug": self.survey.slug})
 
+
 class Option(models.Model):
     question = models.ForeignKey(
-        Question, on_delete=models.CASCADE, related_name='option_question')
+        Question, on_delete=models.CASCADE, related_name="option_question"
+    )
     option = models.CharField(max_length=250)
     selected = models.IntegerField(default=0)
 
@@ -69,4 +72,3 @@ class Option(models.Model):
 
     def get_absolute_url(self):
         return reverse("survey_edit", kwargs={"slug": self.question.survey.slug})
-    
